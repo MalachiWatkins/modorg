fa1d94890c33087bd99710ef570030eefcb208cf
malachi <rustadict1221@gmail.com>
2020-02-28 09:24:18 -0500

    WIP

--- main.py
+++ main.py
@@ -3,12 +3,10 @@ import shutil
 def find (name, path):
     results = []
     for root, dirs, files in os.walk(path):
-        for name in files:
+        if name in files:
             print('yes')
-            if path.endswith(name):
-                results.append(os.path.join(root, name))
-                print('yes')
-                shutil.move(name, 'testfolder')
+            results.append(os.path.join(root, name))
+            shutil.move(name, 'testfolder')
         else:
             print('no')
             return os.path.join(root, name)
