88ecfb0e23fd68209222743afd67f422298590c4
malachi <rustadict1221@gmail.com>
2020-02-28 12:28:07 -0500

    WIP

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import os
 import shutil
 def find (name, path):
     results = []
+    filename = name + '.rar'
     for root, dirs, files in os.walk(path):
-        if name in files:
-            print('yes')
-            results.append(os.path.join(root, name))
-            shutil.move(name, 'testfolder')
-        else:
-            print('no')
-            return os.path.join(root, name)
-find(name = 'abc12', path = '/home/malachi/Desktop/projects/modorg')
+        for x in files:
+            if filename in x:
+                results.append(os.path.join(root, name))
+                shutil.move(x, 'testfolder')
+            else:
+                return os.path.join(root, name)
+find(name = 'weapon', path = '/home/malachi/Desktop/projects/modorg')
