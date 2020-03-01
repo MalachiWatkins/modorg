42878862619cf92123327fb83da3357a2512b5ad
malachi <rustadict1221@gmail.com>
2020-03-01 05:30:05 -0500

    WIP

--- catagories.py
+++ catagories.py
@@ -1,168 +1,38 @@
 import os
 import shutil
 import time
-def category1(file_tag, file_extention, path_to_files, move_folder_path):
-    # adds file extention to file name
-    filename = file_tag + file_extention
+import copy
+import sys
+path_to_files = '/home/malachi/Desktop/projects/modorg'
+move_folder_path = '/home/malachi/Desktop/projects/modorg/testfolder'
+def anotherone(file_name):
+    for root, dirs, files in os.walk(path_to_files):
+        for x in files:
+            if file_name in x:
+                shutil.move(x, move_folder_path)
+                time.sleep(.5)
+            else:
+                exit()
+    return
+def category(pathtofiles, move_folder_path):
+    file_tag = input("Enter an inital file tag to use:  ")
+    exinput = input('Enter a file Extenrtion to use like .rar:  ')
+
+    filename = file_tag + exinput
     for root, dirs, files in os.walk(path_to_files): # gets the path and uses os walk to cycle trough dirs
         for x in files:
             if filename in x: # checks if file name is there
                 shutil.move(x, move_folder_path) # moves selected file to selected directory absolute path needed
-                print('[#              ]') #  progress bar
+                print('yes') #  progress bar
                 time.sleep(.5) # pauses for x seconds befor running second function so everythins wrorks properly
+                anotherone(file_name = filename)
             else:
-                return os.path.join(root, file_tag)
-def category2(file_tag, file_extention, path_to_files, move_folder_path):
-
-    filename = file_tag + file_extention
-    for root, dirs, files in os.walk(path_to_files):
-        for x in files:
-            if filename in x:
-                shutil.move(x, move_folder_path)
-                print('[##             ]')
+                print(filename + '  was not found')
                 time.sleep(.5)
-            else:
+                moreinp = input('Would you like to use another tag (yes:no): ')
+                if moreinp == 'yes':
+                    category(pathtofiles = path_to_files, move_folder_path = move_folder_path)
+                else:
+                    exit()
                 return os.path.join(root, file_tag)
-def category3(file_tag, file_extention, path_to_files, move_folder_path):
-
-    filename = file_tag + file_extention
-    for root, dirs, files in os.walk(path_to_files):
-        for x in files:
-            if filename in x:
-                shutil.move(x, move_folder_path)
-                print('[###            ]')
-                time.sleep(.5)
-            else:
-                return os.path.join(root, file_tag)
-def category4(file_tag, file_extention, path_to_files, move_folder_path):
-
-    filename = file_tag + file_extention
-    for root, dirs, files in os.walk(path_to_files):
-        for x in files:
-            if filename in x:
-                shutil.move(x, move_folder_path)
-                print('[####           ]')
-                time.sleep(.5)
-            else:
-                return os.path.join(root, file_tag)
-def category5(file_tag, file_extention, path_to_files, move_folder_path):
-
-    filename = file_tag + file_extention
-    for root, dirs, files in os.walk(path_to_files):
-        for x in files:
-            if filename in x:
-                shutil.move(x, move_folder_path)
-                print('[#####          ]')
-                time.sleep(.5)
-            else:
-                return os.path.join(root, file_tag)
-def category6(file_tag, file_extention, path_to_files, move_folder_path):
-
-    filename = file_tag + file_extention
-    for root, dirs, files in os.walk(path_to_files):
-        for x in files:
-            if filename in x:
-                shutil.move(x, move_folder_path)
-                print('[######         ]')
-                time.sleep(.5)
-            else:
-                return os.path.join(root, file_tag)
-def category7(file_tag, file_extention, path_to_files, move_folder_path):
-
-    filename = file_tag + file_extention
-    for root, dirs, files in os.walk(path_to_files):
-        for x in files:
-            if filename in x:
-                shutil.move(x, move_folder_path)
-                print('[#######        ]')
-                time.sleep(.5)
-            else:
-                return os.path.join(root, file_tag)
-def category8(file_tag, file_extention, path_to_files, move_folder_path):
-
-    filename = file_tag + file_extention
-    for root, dirs, files in os.walk(path_to_files):
-        for x in files:
-            if filename in x:
-                shutil.move(x, move_folder_path)
-                print('[########       ]')
-                time.sleep(.5)
-            else:
-                return os.path.join(root, file_tag)
-def category9(file_tag, file_extention, path_to_files, move_folder_path):
-
-    filename = file_tag + file_extention
-    for root, dirs, files in os.walk(path_to_files):
-        for x in files:
-            if filename in x:
-                shutil.move(x, move_folder_path)
-                print('[#########      ]')
-                time.sleep(.5)
-            else:
-                return os.path.join(root, file_tag)
-def category10(file_tag, file_extention, path_to_files, move_folder_path):
-
-    filename = file_tag + file_extention
-    for root, dirs, files in os.walk(path_to_files):
-        for x in files:
-            if filename in x:
-                shutil.move(x, move_folder_path)
-                print('[##########     ]')
-                time.sleep(.5)
-            else:
-                return os.path.join(root, file_tag)
-def category11(file_tag, file_extention, path_to_files, move_folder_path):
-
-    filename = file_tag + file_extention
-    for root, dirs, files in os.walk(path_to_files):
-        for x in files:
-            if filename in x:
-                shutil.move(x, move_folder_path)
-                print('[###########    ]')
-                time.sleep(.5)
-            else:
-                return os.path.join(root, file_tag)
-def category12(file_tag, file_extention, path_to_files, move_folder_path):
-
-    filename = file_tag + file_extention
-    for root, dirs, files in os.walk(path_to_files):
-        for x in files:
-            if filename in x:
-                shutil.move(x, move_folder_path)
-                print('[############   ]')
-                time.sleep(.5)
-            else:
-                return os.path.join(root, file_tag)
-def category13(file_tag, file_extention, path_to_files, move_folder_path):
-
-    filename = file_tag + file_extention
-    for root, dirs, files in os.walk(path_to_files):
-        for x in files:
-            if filename in x:
-                shutil.move(x, move_folder_path)
-                print('[#############  ]')
-                time.sleep(.5)
-            else:
-                return os.path.join(root, file_tag)
-def category14(file_tag, file_extention, path_to_files, move_folder_path):
-
-    filename = file_tag + file_extention
-    for root, dirs, files in os.walk(path_to_files):
-        for x in files:
-            if filename in x:
-                shutil.move(x, move_folder_path)
-                print('[############## ]')
-                time.sleep(.5)
-            else:
-                return os.path.join(root, file_tag)
-def category15(file_tag, file_extention, path_to_files, move_folder_path):
-
-    filename = file_tag + file_extention
-    for root, dirs, files in os.walk(path_to_files):
-        for x in files:
-            if filename in x:
-                shutil.move(x, move_folder_path)
-                print('[###############]')
-                time.sleep(.5)
-            else:
-                return os.path.join(root, file_tag)
+category(pathtofiles = path_to_files, move_folder_path = move_folder_path)
---
+++ test.py
@@ -0,0 +1,3 @@
+if inital file tag is found ask if tehy would like to use more as well as the file
+folder path witch could be shortend and apended to the og file
+path
